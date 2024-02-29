a0f780888f6a340d2c837b5908e7b083e4ccad50
2blexn <300amfkall@gmail.com>
2024-03-01 00:58:28 +0200

    DFSD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CarouselComponent],
+  imports: [RouterOutlet, MainComponent, HeaderComponent, 
+		FooterComponent, CarouselComponent, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lr2';
+	ngOnInit(): void {
+    initFlowbite();
+  }
+	links = [
+  {path:'/main', label: 'Main', active: 'button-active'},
+  {path:'/carousel', label: 'About', active: 'button-active'},
+  {path:'/header', label: 'Header', active: 'button-active'} // новий маршрут
+]
+
+/* 	links = [
+	{path:'/main', label: 'Main', active: 'button-active'},
+	{path:'/carousel', label: 'Carousel', active: 'button-active'}
+	] */
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: "main"},
+  {path: 'main', component: MainComponent},
+  {path: 'carousel', component: CarouselComponent},
+  {path: 'header', component: HeaderComponent} // новий маршрут
+];
+
+/* export const routes: Routes = [
+	{path: '', pathMatch: 'full', redirectTo: "main"},
+	{path: 'main', component: MainComponent},
+	{path: 'carousel', component: CarouselComponent}
+];
+ */
